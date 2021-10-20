359ad5ad01489d63439e0c8c1ebd29b1d40a5766
SusuGostoso <92827233+SusuGostoso@users.noreply.github.com>
2021-10-20 00:57:21 -0300

    Versão 0.1 do Projeto

---
+++ Library_main.c
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                 Projeto de Controle de Biblioteca                       ///
+///               Developed by  @SusuGostoso - Out, 2021                    ///
+///             Developed by  @daividfernandoo - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Assinatura das funções
+void telaSobre(void);
+
+// Programa principal
+int main(void)
+{
+    
+    telaSobre();
+    system("pause");
+    return 0;
+}
+
+// Função para exibir informações do projeto
+void telaSobre(void)
+{
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto de Controle de Biblioteca                      ///\n");
+    printf("///               Developed by  @SusuGostoso - Out, 2021                    ///\n");
+    printf("///             Developed by  @daividfernandoo - Out, 2021                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///        = = = = = Projeto de Controle de Biblioteca = = = = =            ///\n");
+    printf("///                             SIG-Library                                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///   Um Sistema de Controle de Biblioteca é o programa responsável por     ///\n");
+    printf("///   controlar os empréstimos de livros aos usuários de uma  biblioteca.   ///\n");
+    printf("///   O presente projeto propõe um sistema capaz de gerir o cadastro dos    ///\n");
+    printf("///   livros e usuários da biblioteca, permitindo o controle e verificação  ///\n");
+    printf("///   dos empréstimos e dos prazos de devolução.                            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
